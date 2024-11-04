5c377a95b47991fc9285428dcb911b556746b930
Brian West <brian@signalwire.com>
2024-11-04 18:14:16 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="signalwire-swaig",
-    version="0.1.9",
+    version="0.1.10",
     include_package_data=True,
     install_requires=[
         "Flask==3.0.3",
