c0ae4d16c0b5296ed7d894184889b17e73cc37d8
Brian West <brian@signalwire.com>
2025-04-24 23:28:24 +0000

    fix typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="signalwire-swaig",
-    version="2.0,
+    version="2.0",
     include_package_data=True,
     install_requires=[
         "Flask",
